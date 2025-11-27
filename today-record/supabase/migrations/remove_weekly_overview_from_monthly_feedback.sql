3f1bc39f5c38c452535c1defb5a9bc5dc54c54bf
goawmfhfl <goawmfhfl1@naver.com>
2025-11-27 17:40:42 +0900

    refactor: 마이그레이션 페이지 및 관련 SQL 파일 삭제 - 사용되지 않는 MigrationPage 컴포넌트와 weekly_overview 컬럼을 monthly_feedback 테이블에서 제거하여 코드 정리 및 유지보수성 향상

--- today-record/supabase/migrations/remove_weekly_overview_from_monthly_feedback.sql
+++
@@ -1,6 +0,0 @@
--- Remove weekly_overview column from monthly_feedback table
--- This column is not used and should be removed
-
-ALTER TABLE monthly_feedback 
-DROP COLUMN IF EXISTS weekly_overview;
-
