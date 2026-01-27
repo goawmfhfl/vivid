7d0c69a366d371d077fdff63753cd1678520d3cc
goawmfhfl <goawmfhfl1@naver.com>
2026-01-27 16:43:19 +0900

    feat: add Kakao share image and enhance metadata for social sharing
    - Introduced a new Kakao share image for improved social media integration.
    - Updated metadata in layout.tsx to include Open Graph and Twitter card information, enhancing the app's visibility and presentation on social platforms.

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -8,6 +8,25 @@ import { Footer } from "../components/common/Footer";
 export const metadata: Metadata = {
   title: "VIVID",
   description: "오늘의 기록을 남기고 분석하는 일기 앱",
+  metadataBase: new URL("https://vividlog.app"),
+  openGraph: {
+    title: "당신의 꿈을 '선명한 확신'으로",
+    description: "기록을 통해, 흐릿한 삶을 선명하게",
+    siteName: "VIVID",
+    url: "/",
+    type: "website",
+    images: [
+      {
+        url: "/kakao-share-image.jpg",
+      },
+    ],
+  },
+  twitter: {
+    title: "당신의 꿈을 '선명한 확신'으로",
+    description: "기록을 통해, 흐릿한 삶을 선명하게.",
+    card: "summary_large_image",
+    images: ["/kakao-share-image.jpg"],
+  },
   icons: {
     icon: [
       { url: "/favicon/favicon-96x96.png", sizes: "96x96", type: "image/png" },
